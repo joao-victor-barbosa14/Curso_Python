d5c5de2b71e44012c45224b2ed7b611bb8ed5b54
João Victor Ferreira Barbosa <joao.v.ferreira.barbosa@gmail.com>
2024-09-08 21:11:34 -0300

    Add Seção 3

---
+++ Seção_03/aula001.py
@@ -0,0 +1,7 @@
+"""
+Isso é uma DocString
+"""
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(456)
---
+++ Seção_03/aula003.py
@@ -0,0 +1,14 @@
+"""
+Python = Linguagem de pregramação 
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão de aspas
+"""
+# Aspas simples
+print('João Victor')
+# Aspas duplas 
+print("João Victor")
+# Escape
+print("João \'Victor\'")
+# r
+print(r"João \'Victor\'")
