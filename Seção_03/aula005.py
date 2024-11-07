80365b2475f03629c2febc1551556985f6d24936
João Victor Ferreira Barbosa <joao.v.ferreira.barbosa@gmail.com>
2024-11-06 21:04:33 -0300

    Tipos de dados bolean

---
+++ Seção_03/aula005.py
@@ -0,0 +1,11 @@
+"""
+Tipo de dados bool (bolean)
+Ao questionar algo em um programa,
+só existem duas respostas possíveis:
+sim (True) ou não (False).
+Existem vários operadores para "questionar".
+Dentre eles o ==, que é um operador lógico que
+questiona se um valor é igual a outro
+"""
+print(10 != 11)
+print(10 == 11)
