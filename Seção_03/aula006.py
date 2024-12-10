d3a5d196f7ad0d67bd7b30769329d7e1bd81df5b
João Victor Ferreira Barbosa <joao.v.ferreira.barbosa@gmail.com>
2024-12-10 20:49:45 -0300

    Conversão de tipos

---
+++ Seção_03/aula006.py
@@ -0,0 +1,8 @@
+"""
+conversão de tipos, coerção
+type convertion, typecasting, coercion
+é o ato de converter um tipo em outro
+tipos primitivimos:
+str, int, float, bool
+"""
+print(int('1')), type(int(('1')))
