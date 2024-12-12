4d797b1c34bbb27554fd2e089b0a8b3562678c61
João Victor Ferreira Barbosa <joao.v.ferreira.barbosa@gmail.com>
2024-12-12 20:08:43 -0300

    Exercício variáveis

---
+++ Seção_03/aula008.py
@@ -0,0 +1,15 @@
+"""
+Exercício variáveis
+"""
+nome = 'João Victor'
+sobrenome = 'Ferreira Barbosa'
+idade = 14
+ano_nascimento = 2010
+maior_idade = idade >= 18
+Altura_metros = 1.68
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano nascimento:', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros:', Altura_metros)
