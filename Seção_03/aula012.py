2be1732d61c4c309700b6542a3ea6a22407a9e62
João Victor Ferreira Barbosa <joao.v.ferreira.barbosa@gmail.com>
2024-12-12 20:32:45 -0300

    Execício cálculo do IMC

---
+++ Seção_03/aula012.py
@@ -0,0 +1,11 @@
+"""
+Execício cálculo do IMC
+"""
+nome = 'João Victor'
+altura = 1.68
+peso = 61
+imc = peso / (altura * altura)
+print('Nome:', nome)
+print('Altura:', altura)
+print('Peso:', peso)
+print('Seu IMC:', imc)
