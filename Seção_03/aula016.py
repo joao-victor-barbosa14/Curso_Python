d20ed520553ea3508cbc7b8c329385a477512dd2
João Victor Ferreira Barbosa <joao.v.ferreira.barbosa@gmail.com>
2024-12-15 20:05:21 -0300

    Introdução á if/elif/else

---
+++ Seção_03/aula016.py
@@ -0,0 +1,13 @@
+"""
+Intrudução á blocos de código
+if/ elif      / else
+se/ se não se / se não 
+"""
+entrada = input('Você deseja entra ou sair? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema!')
+elif entrada == 'sair':
+    print('Você saiu...')
+else:
+    print('Você não digitou nem entrar e nem sair!')
