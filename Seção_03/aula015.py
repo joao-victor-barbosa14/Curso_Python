e6d6e1e7dc5b775c28dc76557b2413a2575ff433
João Victor Ferreira Barbosa <joao.v.ferreira.barbosa@gmail.com>
2024-12-15 19:55:54 -0300

    Função input

---
+++ Seção_03/aula015.py
@@ -0,0 +1,10 @@
+"""
+Função input
+"""
+numero01 = int(input('Digite um número: '))
+numero02 = int(input('Digite outro número: '))
+print(f'A soma dos valores é: {numero01 + numero02}')
+
+# intnumero = int(numero01)
+# intnumero2 = int(numero02)
+# print(f'A soma dos números é: {intnumero + intnumero2}')
