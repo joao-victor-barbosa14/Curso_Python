469ea907cb1be0f8ce75d544e2f45c3d22b98dab
João Victor Ferreira Barbosa <joao.v.ferreira.barbosa@gmail.com>
2024-12-15 19:43:07 -0300

    F-strings

--- Seção_03/aula012.py
+++ Seção_03/aula012.py
@@ -5,7 +5,6 @@ nome = 'João Victor'
 altura = 1.68
 peso = 61
 imc = peso / (altura * altura)
-print('Nome:', nome)
-print('Altura:', altura)
-print('Peso:', peso)
-print('Seu IMC:', imc)
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
