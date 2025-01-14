9ad18da221bd6510fa79e73672caf4153593e744
João Victor Ferreira Barbosa <joao.v.ferreira.barbosa@gmail.com>
2025-01-14 20:37:37 -0300

    Operadores de comparação

--- Seção_03/aula019.py
+++ Seção_03/aula019.py
@@ -8,7 +8,7 @@ OP      Significado         Exemplo (True)
 ==      igual               'a' == 'a'
 !=      diferente           'a' != 'b'
 """
-maior = 2 > 1
+maior = 3 > 1
 maior_ou_igual = 2 >= 2
 menor = 1 < 2
 menor_ou_igual = 2 <= 2
