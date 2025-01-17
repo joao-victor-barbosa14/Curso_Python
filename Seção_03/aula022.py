ca9d13e0b07fb11c287f46101a02912195ca29cd
João Victor Ferreira Barbosa <joao.v.ferreira.barbosa@gmail.com>
2025-01-16 22:04:55 -0300

    Introdução á or

---
+++ Seção_03/aula022.py
@@ -0,0 +1,19 @@
+"""
+Operadores lógicos 
+and (e)  or (ou) nor (não)
+and - Todas as condições precisam ser
+verdadeiras.
+Se qualquer valor for considerado falso,
+a expressão inteira será avaliada naquele valor
+São considerados falsy (que você já viu)
+0 0.0 '' False
+Também existe o tipo None que é
+usando para representar um não valor
+"""
+entrada = input('[E]entrar [S]sair: ')
+senha_digitada = input('Senha: ')
+
+if (entrada == 'E' or entrada == 'e') and senha_digitada == 'Jv2010':
+    print('Você entrou...')
+else:
+    ('Saindo...')
