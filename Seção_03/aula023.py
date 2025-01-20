39da67ceb12f0222b8d605857f45b7a97e1b6631
João Victor Ferreira Barbosa <joao.v.ferreira.barbosa@gmail.com>
2025-01-20 19:36:08 -0300

    Operador lógico not

---
+++ Seção_03/aula023.py
@@ -0,0 +1,11 @@
+"""
+Operador lógico "not"
+Usando para inverter expressões
+not True = False
+not False = True
+"""
+digitou = input('Digite a senha: ')
+senha = 'Jv2010'
+
+if not digitou:
+    print('Você não digitou nada')
