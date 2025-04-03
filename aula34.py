486303cbce4964fe709578ce7c786000110aee34
João Victor Ferreira Barbosa <joao.v.ferreira.barbosa@gmail.com>
2025-04-03 20:03:23 -0300

    Introdução á While

---
+++ aula34.py
@@ -0,0 +1,13 @@
+"""
+Repetições
+while(enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+"""
+condicao = True
+
+while condicao:
+    nome = input('Qual é seu nome? ')
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break
